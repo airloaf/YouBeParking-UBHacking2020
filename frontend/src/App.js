328d38ae32a425ef9fa639b1974b8b4566fc66c9
Vikram Singh <vikzilla18@gmail.com>
2020-10-24 13:22:26 -0400

    Basic landing page in React.js

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Navigation from './components/Navigation';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
