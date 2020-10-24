8c8a5ea0dd3b56a48d95ba1c36d3e7ea8669f93b
Vikram Singh <vikzilla18@gmail.com>
2020-10-24 16:18:50 -0400

    Created login and register page with Formik

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,55 +1,89 @@
 import React from 'react'
 
-import {ButtonGroup, Button, Col, Row, Form} from 'react-bootstrap'
+import { ButtonGroup, Button, Col, Row, Form } from 'react-bootstrap'
+import { Formik } from 'formik'
 
 export default class Login extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             username: "",
-            password: "" 
+            password: ""
         };
-        this.handleChange = this.handleChange.bind(this);
+        this.validate = this.validate.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
 
-    handleChange(e){
-        console.log(e.target.id);
-        console.log(e.target.value);
-        var newState = {};
-        newState[e.target.id] = e.target.value;
-        this.setState(newState);
+    validate(values) {
+        const errors = {};
+        if (!values.username) {
+            errors.username = 'Required';
+        }
+        if (!values.password) {
+            errors.password = "Required";
+        }
+        return errors;
+    }
+
+    onSubmit(values, { setSubmitting }) {
+        console.log(JSON.stringify(values, null, 2));
+        setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+        }, 400);
     }
 
     render() {
         return (
             <React.Fragment>
-                <Form className="form-signin">
-                    <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-                    <Form.Group controlId="username">
-                        <Row>
-                            <Col sm="1" />
-                            <Form.Label column sm="2">username</Form.Label>
-                            <Col sm="8">
-                                <Form.Control controlid="username" type="username" placeholder="Enter username" onChange={this.handleChange} />
-                            </Col>
-                            <Col sm="1" />
-                        </Row>
-                    </Form.Group>
-                    <Form.Group controlId="password">
-                        <Row>
-                            <Col sm="1" />
-                            <Form.Label column sm="2">Password</Form.Label>
-                            <Col sm="8">
-                                <Form.Control type="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} />
-                            </Col>
-                            <Col sm="1" />
-                        </Row>
-                    </Form.Group>
-                </Form>
-                <ButtonGroup>
-                    <Button variant="primary" type="submit" onSubmit={()=>{console.log("Submitted")}}>Sign in</Button>
-                    <Button variant="primary" href="register">Register</Button>
-                </ButtonGroup>
+                <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+                <Formik
+                    initialValues={{
+                        username: '',
+                        password: ''
+                    }}
+                    validate={this.validate}
+                    onSubmit={this.onSubmit}
+                >
+                    {({
+                        values,
+                        errors,
+                        touched,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                        isSubmitting,
+                    }) => (
+                            <Form onSubmit={handleSubmit}>
+                                <Form.Row>
+                                    <Col sm="1" />
+                                    <Form.Label column sm="2">username</Form.Label>
+                                    <Col sm="8">
+                                        <Form.Control type="username" name="username" placeholder="Username" onChange={handleChange} onBlur={handleBlur} value={values.username} isInvalid={!!errors.username} />
+                                        <Form.Control.Feedback type="invalid">
+                                            {errors.username}
+                                        </Form.Control.Feedback>
+                                    </Col>
+                                    <Col sm="1" />
+                                </Form.Row>
+                                <Form.Row>
+                                    <Col sm="1" />
+                                    <Form.Label column sm="2">Password</Form.Label>
+                                    <Col sm="8">
+                                        <Form.Control type="password" placeholder="Password" name="password" onChange={handleChange} onBlur={handleBlur} value={values.password} isInvalid={!!errors.username} />
+                                        <Form.Control.Feedback type="invalid">
+                                            {errors.password}
+                                        </Form.Control.Feedback>
+                                    </Col>
+                                    <Col sm="1" />
+                                </Form.Row>
+                                <ButtonGroup>
+                                    <Button variant="primary" type="submit" disabled={isSubmitting} >Sign in</Button>
+                                    <Button variant="primary" href="register" >Register</Button>
+                                </ButtonGroup>
+                            </Form>
+                        )}
+                </Formik>
             </React.Fragment>
         )
     }
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import {Button, Col, Row, Form} from 'react-bootstrap'
+import { ButtonGroup, Button, Col, Row, Form } from 'react-bootstrap'
+import { Formik } from 'formik'
 
 export default class Register extends React.Component {
     constructor(props) {
@@ -11,60 +12,116 @@ export default class Register extends React.Component {
             password: null,
             confirmPassword: null,
         };
+
+        this.validate = this.validate.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    validate(values) {
+        const errors = {};
+        if (!values.email) {
+            errors.email= 'Required';
+        }
+        if (!values.username) {
+            errors.username = 'Required';
+        }
+        if (!values.password) {
+            errors.password = "Required";
+        }
+        if (!values.confirmPass) {
+            errors.confirmPass = "Required";
+        }
+        if(values.confirmPass != values.password){
+            errors.confirmPass = "Passwords do not match!"
+        }
+        return errors;
+    }
+
+    onSubmit(values, { setSubmitting }) {
+        console.log(JSON.stringify(values, null, 2));
+        setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+        }, 400);
     }
 
     render() {
         return (
             <React.Fragment>
-                <Form class="form-signin">
-                    <h1 class="h3 mb-3 font-weight-normal">Please Register</h1>
-                    <Form.Group>
-                        <Row>
-                            <Col sm="1" />
-                            <Form.Label column sm="2">Email Address</Form.Label>
-                            <Col sm="8">
-                                <Form.Control type="email" placeholder="Enter email" />
-                            </Col>
-                            <Col sm="1" />
-                        </Row>
-                    </Form.Group>
-                    <br />
-                    <Form.Group>
-                        <Row>
-                            <Col sm="1" />
-                            <Form.Label column sm="2">Username</Form.Label>
-                            <Col sm="8">
-                                <Form.Control type="username" placeholder="Enter username" />
-                            </Col>
-                            <Col sm="1" />
-                        </Row>
-                    </Form.Group>
-                    <br />
-                    <Form.Group controlId="formBasicPassword">
-                        <Row>
-                            <Col sm="1" />
-                            <Form.Label column sm="2">Password</Form.Label>
-                            <Col sm="8">
-                                <Form.Control type="password" placeholder="Password" />
-                            </Col>
-                            <Col sm="1" />
-                        </Row>
-                    </Form.Group>
-                    <Form.Group controlId="formBasicPassword">
-                        <Row>
-                            <Col sm="1" />
-                            <Form.Label column sm="2">Confirm Password</Form.Label>
-                            <Col sm="8">
-                                <Form.Control type="password" placeholder="Confirm Password" />
-                            </Col>
-                            <Col sm="1" />
-                        </Row>
-                        <Form.Text className="text-muted">
-                            Password has a minimum of 6 characters and a maximum of 20
-                        </Form.Text>
-                    </Form.Group>
-                </Form>
-                <Button variant="primary" type="submit">Register</Button>
+                <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+                <Formik
+                    initialValues={{
+                        email: '',
+                        username: '',
+                        password: '',
+                        confirmPass: ''
+                    }}
+                    validate={this.validate}
+                    onSubmit={this.onSubmit}
+                >
+                    {({
+                        values,
+                        errors,
+                        touched,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                        isSubmitting,
+                    }) => (
+                            <Form onSubmit={handleSubmit}>
+                                <Form.Row>
+                                    <Col sm="1" />
+                                    <Form.Label column sm="2">email</Form.Label>
+                                    <Col sm="8">
+                                        <Form.Control type="email" name="email" placeholder="example@email.com" onChange={handleChange} onBlur={handleBlur} value={values.email} isInvalid={!!errors.email} />
+                                        <Form.Control.Feedback type="invalid">
+                                            {errors.email}
+                                        </Form.Control.Feedback>
+                                    </Col>
+                                    <Col sm="1" />
+                                </Form.Row>
+                                <br />
+                                <Form.Row>
+                                    <Col sm="1" />
+                                    <Form.Label column sm="2">username</Form.Label>
+                                    <Col sm="8">
+                                        <Form.Control type="username" name="username" placeholder="Username" onChange={handleChange} onBlur={handleBlur} value={values.username} isInvalid={!!errors.username} />
+                                        <Form.Control.Feedback type="invalid">
+                                            {errors.username}
+                                        </Form.Control.Feedback>
+                                    </Col>
+                                    <Col sm="1" />
+                                </Form.Row>
+                                <br />
+                                <Form.Row>
+                                    <Col sm="1" />
+                                    <Form.Label column sm="2">Password</Form.Label>
+                                    <Col sm="8">
+                                        <Form.Control type="password" placeholder="Password" name="password" onChange={handleChange} onBlur={handleBlur} value={values.password} isInvalid={!!errors.username} />
+                                        <Form.Control.Feedback type="invalid">
+                                            {errors.password}
+                                        </Form.Control.Feedback>
+                                    </Col>
+                                    <Col sm="1" />
+                                </Form.Row>
+                                <br />
+                                <Form.Row>
+                                    <Col sm="1" />
+                                    <Form.Label column sm="2">Confirm Password</Form.Label>
+                                    <Col sm="8">
+                                        <Form.Control type="password" placeholder="Confirm Password" name="confirmPass" onChange={handleChange} onBlur={handleBlur} value={values.confirmPass} isInvalid={!!errors.confirmPass} />
+                                        <Form.Control.Feedback type="invalid">
+                                            {errors.confirmPass}
+                                        </Form.Control.Feedback>
+                                    </Col>
+                                    <Col sm="1" />
+                                </Form.Row>
+                                <br />
+                                <Button variant="primary" type="submit" disabled={isSubmitting} >Register</Button>
+                            </Form>
+                        )}
+                </Formik>
+
             </React.Fragment>
         )
     }
