daf72ca0e43a4ee580edde8df9c68c064f373831
Vikram Singh <vikzilla18@gmail.com>
2020-10-25 05:43:24 -0400

    Added map to offer verify

--- frontend/src/pages/OfferVerify.js
+++ frontend/src/pages/OfferVerify.js
@@ -1,59 +1,71 @@
 import React from 'react'
-import { Offer } from '../redux/Actions'; 
+import { Offer } from '../redux/Actions';
 import { compose } from 'recompose'
 import { connect } from 'react-redux'
 import { withRouter } from "react-router-dom";
-import { Button, ListGroup ,Container, Row,Col} from "react-bootstrap"
+import { Button, ListGroup, Container, Row, Col, Form } from "react-bootstrap"
 import axios from "axios"
 
+import Iframe from 'react-iframe'
+import mapDict from "../utils/LotFrames";
+
 class OfferVerify extends React.Component {
-    constructor(props){
-        super(props); 
-    }
+  constructor(props) {
+    super(props);
+  }
 
 
   submitForm() {
     console.log(this.props.User.offer.offer_id)
-  axios.post('/users/offerSuccess', {
-    offer_id: this.props.User.offer.offer_id,
-  })
-    .then((response) => {
-      console.log(response)
+    axios.post('/users/offerSuccess', {
+      offer_id: this.props.User.offer.offer_id,
     })
-    .catch((err)=> {
-      console.log(err)
-    })
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
 
   }
 
 
-render(){
-     return (
+  render() {
+    return (
       <React.Fragment>
-      <ListGroup>
-     
-      <ListGroup.Item variant = 'primary' action onClick={() => this.submitForm()}>
-      <Button variant="primary">Complete Transaction</Button>{' '}
-      </ListGroup.Item>
-      
-      </ListGroup>
+        <ListGroup>
+
+      <Form.Label><h2>{this.props.User.offer.lot}</h2></Form.Label>
+          <Iframe
+            url={mapDict[this.props.User.offer.lot]}
+            width="100%"
+            height="450px"
+            display="initial"
+            position="relative"
+          />
+          <h1>{this.props.User.offer.description}</h1>
+          <ListGroup.Item variant='primary' action onClick={() => this.submitForm()}>
+            <Button variant="primary">Complete Transaction</Button>{' '}
+          </ListGroup.Item>
+
+        </ListGroup>
       </React.Fragment>
-     )
-    }
- 
+    )
+  }
+
 }
 
 
 const mapStateToProps = (state) => {
-  const {User} = state;
+  const { User } = state;
   return {
-      User
+    User
   }
 }
 
-const mapDispatchToProps = {Offer}
+const mapDispatchToProps = { Offer }
 
 export default connect(mapStateToProps, mapDispatchToProps)
-(
-  compose(withRouter)(OfferVerify)
-)
+  (
+    compose(withRouter)(OfferVerify)
+  )
