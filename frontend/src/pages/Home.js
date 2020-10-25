1a2bb08b325b18121a8b2090556f640b6df0b0ef
TravisBute <trb0814@gmail.com>
2020-10-25 01:48:40 -0400

    adjust landing page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import '../styles.css';
 
 export default class Home extends React.Component {
     constructor(props) {
@@ -9,95 +8,19 @@ export default class Home extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <header class="masthead bg-primary text-white text-center">
+                <header class="masthead bg-success text-white text-center">
                     <div class="container d-flex align-items-center flex-column">
-                        <img class="masthead-avatar mb-5" src="assets/img/avataaars.svg" alt="" />
-                        <h1 class="masthead-heading text-uppercase mb-0">YouBeParking!</h1>
-                        <div class="divider-custom divider-light">
-                            <div class="divider-custom-line"></div>
-                            <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                            <div class="divider-custom-line"></div>
-                        </div>
-                        <p class="masthead-subheading font-weight-light mb-0">A parksharing service for UB's North Campus</p>
+                        <h1 class="masthead-heading mb-0"><br/>YouBeParking!</h1>
+                        <p class="masthead-subheading font-weight-light mb-5">A parksharing service for UB's North Campus</p>
                     </div>
                 </header>
-                <section class="page-section portfolio" id="portfolio">
+                <section class="page-section portfolio m-3" id="portfolio" >
                     <div class="container">
-                        <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">How it works</h2>
-                        <div class="divider-custom">
-                            <div class="divider-custom-line"></div>
-                            <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                            <div class="divider-custom-line"></div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6 col-lg-4 mb-5">
-                                <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal1">
-                                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                        <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
-                                    </div>
-                                    <img class="img-fluid" src="assets/img/portfolio/cabin.png" alt="" />
-                                </div>
-                            </div>
-                            <div class="col-md-6 col-lg-4 mb-5">
-                                <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal2">
-                                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                        <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
-                                    </div>
-                                    <img class="img-fluid" src="assets/img/portfolio/cake.png" alt="" />
-                                </div>
-                            </div>
-                            <div class="col-md-6 col-lg-4 mb-5">
-                                <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal3">
-                                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                        <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
-                                    </div>
-                                    <img class="img-fluid" src="assets/img/portfolio/circus.png" alt="" />
-                                </div>
-                            </div>
-                            <div class="col-md-6 col-lg-4 mb-5 mb-lg-0">
-                                <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal4">
-                                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                        <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
-                                    </div>
-                                    <img class="img-fluid" src="assets/img/portfolio/game.png" alt="" />
-                                </div>
-                            </div>
-                            <div class="col-md-6 col-lg-4 mb-5 mb-md-0">
-                                <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal5">
-                                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                        <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
-                                    </div>
-                                    <img class="img-fluid" src="assets/img/portfolio/safe.png" alt="" />
-                                </div>
-                            </div>
-                            <div class="col-md-6 col-lg-4">
-                                <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal6">
-                                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                        <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
-                                    </div>
-                                    <img class="img-fluid" src="assets/img/portfolio/submarine.png" alt="" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <section class="page-section bg-primary text-white mb-0" id="about">
-                    <div class="container">
-                        <h2 class="page-section-heading text-center text-uppercase text-white">About</h2>
-                        <div class="divider-custom divider-light">
-                            <div class="divider-custom-line"></div>
-                            <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                            <div class="divider-custom-line"></div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-4 ml-auto"><p class="lead">Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p></div>
-                            <div class="col-lg-4 mr-auto"><p class="lead">You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p></div>
-                        </div>
-                        <div class="text-center mt-4">
-                            <a class="btn btn-xl btn-outline-light" href="https://startbootstrap.com/themes/freelancer/">
-                                <i class="fas fa-download mr-2"></i>
-                            </a>
-                        </div>
+                        <h2 class="page-section-heading text-center text-secondary m-5">How It Works</h2>
+                        <p class="masthead-subheading font-weight-light mb-0">Offer up your parking spot for people who need it.</p>
+                        <p class="masthead-subheading font-weight-light mb-0">- or -</p>
+                        <p class="masthead-subheading font-weight-light mb-5">Request a spot from the list of current offers.</p>
+                        <h1 class="page-section-heading text-center text-secondary mt-3">Create an account and start sharing!</h1>
                     </div>
                 </section>
             </React.Fragment>
--- frontend/src/pages/Offer.js
+++ frontend/src/pages/Offer.js
@@ -14,12 +14,16 @@ export default class Home extends React.Component {
                     <Col></Col>
                     <Col xs={6}>
                         <Form.Group controlId="exampleForm.ControlInput1">
-                            <Form.Label><h2>Lot</h2></Form.Label>
-                            <Form.Control type="email" placeholder="Which parking lot are you in?" />
+                            <Form.Label><h2><br/><br/>Lot</h2></Form.Label>
+                            <Form.Control placeholder="Which parking lot are you in?" />
                         </Form.Group>
                         <Form.Group controlId="exampleForm.ControlTextarea1">
-                            <Form.Label><h2>Description of your spot</h2></Form.Label>
-                            <Form.Control as="textarea" rows={3} />
+                            <Form.Label><h2>Description</h2></Form.Label>
+                            <Form.Control as="textarea" placeholder="give a brief description of your spot's location." rows={3} />
+                        </Form.Group>
+                        <Form.Group controlId="exampleForm.ControlInput1">
+                            <Form.Label><h2>Time</h2></Form.Label>
+                            <Form.Control placeholder="what time will you leave?" />
                         </Form.Group>
                         <div><Button href="/offer" variant="outline-success" block><h1>Offer a Spot</h1></Button></div>
                     </Col>
